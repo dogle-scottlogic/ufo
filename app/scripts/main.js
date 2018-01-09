2a300fda1d80d16833df4e0c5f5ad3627a6f0b3e
Dave Ogle <dogle@scottlogic.co.uk>
2018-01-09 15:50:45 +0000

    Add data and basic blobs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,10 +7,52 @@ var svg = d3.select('#chart').append('svg')
     .attr('width', width)
     .attr('height', height);
 
-d3.json('scripts/counties.json', (error, us) => {
+d3.json("scripts/states.json", function (error, us) {
     if (error) return console.error(error);
 
-    svg.append('path')
+    svg.append("path")
         .datum(topojson.mesh(us))
-        .attr('d', path);
+        .attr("class", "land")
+        .attr("d", path);
+
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function (a, b) {
+            return a !== b;
+        }))
+        .attr("class", "border border--state")
+        .attr("d", path);
+
+    var radius = d3.scale.sqrt()
+        .domain([0, 1e6])
+        .range([0, 500]);
+
+    svg.append("g")
+        .attr("class", "bubble")
+        .selectAll("circle")
+        .data(topojson.feature(us, us.objects.states).features
+            .sort((a, b) => b.properties.sightings - a.properties.sightings))
+        .enter().append("circle")
+        .attr("transform", function (d) {
+            return "translate(" + path.centroid(d) + ")";
+        })
+        .attr("r", function (d) {
+            
+            return d.properties.sightings ? radius(d.properties.sightings) : 0;
+        });
+    
+    // var legend = svg.append("g")
+    //     .attr("class", "legend")
+    //     .attr("transform", "translate(" + (width - 50) + "," + (height - 20) + ")")
+    //     .selectAll("g")
+    //     .data([1e6, 3e6, 6e6])
+    //     .enter().append("g");
+
+    // legend.append("circle")
+    //     .attr("cy", function (d) { return -radius(d); })
+    //     .attr("r", radius);
+
+    // legend.append("text")
+    //     .attr("y", function (d) { return -2 * radius(d); })
+    //     .attr("dy", "1.3em")
+    //     .text(d3.format(".1s"));
 });
--- makefile
+++ makefile
@@ -2,16 +2,14 @@ build/cb_2016_us_county_20m.zip:
 	mkdir -p $(dir $@)
 	curl -o $@ https://www2.census.gov/geo/tiger/GENZ2016/shp/$(notdir $@)
 
-build/cb_2016_us_county_20m.shp: build/cb_2016_us_county_20m.zip
+build/cb_2016_us_state_20m.shp: build/cb_2016_us_state_20m.zip
 	unzip -od $(dir $@) $<
 	touch $@
 
 # Not currently working - use commandline instead
-build/counties.json: build/cb_2016_us_county_20m.shp
-	topojson \
-		-o $@ \
-		--projection='width = 960, height = 600, d3.geo.albersUsa() \
-			.scale(1280) \
-			.translate([width / 2, height / 2])' \
-		--simplify=.5 \
-		-- counties=$<
+build/states.json: build/cb_2016_us_state_20m.shp build/ufo_sightings_by_state.csv
+	topojson -o build/states.json --id-property='STATEFP,id' --external-properties=build/ufo_sightings_by_state.csv \
+	--properties='name=state' \
+	--properties='sightings=count' \
+	--projection='width = 960, height = 600, d3.geo.albersUsa().scale(1280).translate([width / 2, height / 2])' --simplify=.5 \
+	-- states=build/cb_2016_us_state_20m.shp
